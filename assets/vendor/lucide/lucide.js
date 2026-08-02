be589489d6546cd047a235feb6f692b6efe57fd6
Yuliia Voziian <vozianulia@gmail.com>
2026-08-03 00:49:00 +0200

    +

--- assets/vendor/lucide/lucide.js
+++ assets/vendor/lucide/lucide.js
@@ -7,7 +7,10 @@
     "chevron-right": '<path d="m9 18 6-6-6-6"/>',
     "plus": '<path d="M12 5v14M5 12h14"/>',
     "check": '<path d="m5 12 4 4L19 6"/>',
-    "mail": '<rect x="3" y="5" width="18" height="14" rx="2"/><path d="m3 7 9 6 9-6"/>'
+    "mail": '<rect x="3" y="5" width="18" height="14" rx="2"/><path d="m3 7 9 6 9-6"/>',
+    "clipboard-list": '<rect x="8" y="2" width="8" height="4" rx="1"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/><path d="M8 11h8"/><path d="M8 16h8"/><path d="M6 11h.01"/><path d="M6 16h.01"/>',
+    "zap": '<path d="M13 2 3 14h8l-1 8 11-14h-8z"/>',
+    "route": '<circle cx="6" cy="19" r="3"/><path d="M9 19h8.5a3.5 3.5 0 0 0 0-7H6.5a3.5 3.5 0 0 1 0-7H15"/><circle cx="18" cy="5" r="3"/>'
   };
   window.lucide = {
     createIcons: function () {
